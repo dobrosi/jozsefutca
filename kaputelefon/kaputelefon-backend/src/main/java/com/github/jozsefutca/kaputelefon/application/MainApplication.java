47cd3c87a65ce65e3e641ae9576ebab9303d6a9b
p9yp9y <p9yp9y@gmail.com>
2021-03-28 09:35:36 +0200

    commit

--- kaputelefon/kaputelefon-backend/src/main/java/com/github/jozsefutca/kaputelefon/application/MainApplication.java
+++ kaputelefon/kaputelefon-backend/src/main/java/com/github/jozsefutca/kaputelefon/application/MainApplication.java
@@ -1,25 +1,27 @@
 package com.github.jozsefutca.kaputelefon.application;
 
-import static org.slf4j.LoggerFactory.getLogger;
-import static org.springframework.boot.SpringApplication.run;
-import java.util.Iterator;
-import javax.annotation.PostConstruct;
-import javax.transaction.Transactional;
-import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import com.github.jozsefutca.kaputelefon.configuration.ApplicationConfiguration;
 import com.github.jozsefutca.kaputelefon.configuration.SpringFoxConfig;
 import com.github.jozsefutca.kaputelefon.model.Settings;
 import com.github.jozsefutca.kaputelefon.repository.SettingsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
+import java.util.Iterator;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 @Import({ApplicationConfiguration.class, SpringFoxConfig.class})
 public class MainApplication {
-  Logger logger = getLogger(MainApplication.class);
+  @Autowired private final SettingsRepository settingsRepository;
 
-  @Autowired private SettingsRepository settingsRepository;
+  public MainApplication(SettingsRepository settingsRepository) {
+    this.settingsRepository = settingsRepository;
+  }
 
   public static void main(String[] args) {
     run(MainApplication.class, args);
@@ -27,7 +29,7 @@ public class MainApplication {
 
   @PostConstruct
   @Transactional
-  protected void init() {
+  public void init() {
     Iterator<Settings> settings = settingsRepository.findAll().iterator();
     if (!settings.hasNext()) {
       settingsRepository.save(new Settings());
