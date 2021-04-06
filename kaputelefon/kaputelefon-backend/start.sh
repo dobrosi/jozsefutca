02759e6b132303e51eb136257ad2fff1f9f267f3
Andras Dobrosi <dobrosi@gmail.com>
2021-04-06 08:56:46 +0200

    start script fix

--- kaputelefon/kaputelefon-backend/start.sh
+++ kaputelefon/kaputelefon-backend/start.sh
@@ -1,1 +1,1 @@
-java -jar target/kaputelefon-backend-0.0.1-SNAPSHOT.jar --spring.config.location=file:~/.config/kaputelefon/application.properties
+java -jar target/kaputelefon-backend-0.0.1-SNAPSHOT.jar --spring.config.location=file:///home/dobrosi/.config/kaputelefon/application.properties
