33dc874ffa13bdc701600087d90bedeb6d65c904
Andras Dobrosi <dobrosi@gmail.com>
2021-04-06 12:55:17 +0200

    swagger conf

--- kaputelefon/kaputelefon-backend/start.sh
+++ kaputelefon/kaputelefon-backend/start.sh
@@ -1,5 +1,6 @@
 java \
 -Dspring.datasource.url=jdbc:h2:file://$HOME/.config/kaputelefon/kaputelefon:kaputelefondb \
 -Dspring.jpa.hibernate.ddl-auto=update \
+-Dspringfox.documentation.swagger.v2.host=kaputelefon.no-ip.hu \
 -jar target/kaputelefon-backend-0.0.1-SNAPSHOT.jar \
 
