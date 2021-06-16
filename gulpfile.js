aba40d3509dd4e4f7faec94bd426ea1f28a6c590
Dobrosi Andras <dobrosi@gmail.com>
2021-06-16 06:45:37 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var inlinesource = require('gulp-inline-source');
  
 gulp.task('inlinesource', function () {
-	    return gulp.src('./src/*.html')
+	    return gulp.src('./kaputelefon/kaputelefon-frontend/src/*.html')
 	        .pipe(inlinesource())
-	        .pipe(gulp.dest('./target'));
+	        .pipe(gulp.dest('./kaputelefon/kaputelefon-frontend/target'));
 });
