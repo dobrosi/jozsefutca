310691a3b9894093552a50442d377228bd9566f6
dobrosi <dobrosi@gmail.com>
2021-06-16 18:28:02 +0200

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,17 +3,15 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  git remote set-url origin https://dobrosi:${GH_TOKEN}@github.com/dobrosi/jozsefutca.git
 }
 
 commit() {
-  git add . *.html
+  git add kaputelefon/kaputelefon-frontend/target/*.html
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote set-url origin https://dobrosi:${GH_TOKEN}@github.com/dobrosi/jozsefutca.git
-  git add kaputelefon/kaputelefon-frontend/target/*.html
-  git commit -m "target"
   git push
 }
 
