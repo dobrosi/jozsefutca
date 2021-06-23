b88389ff28aca93cbd867fe1917e6c285426f839
Travis CI <travis@travis-ci.org>
2021-06-23 13:00:24 +0200

    commit

--- work/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var inlinesource = require('gulp-inline-source');
- 
-gulp.task('inlinesource', function () {
-	    return gulp.src('../kaputelefon/kaputelefon-frontend/src/*.html')
-	        .pipe(inlinesource())
-	        .pipe(gulp.dest('../kaputelefon/kaputelefon-frontend/target'));
-});
