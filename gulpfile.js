cd7206f421fd899e0abc38591a1546db4f3b3bec
Dobrosi Andras <dobrosi@gmail.com>
2021-11-04 13:29:50 +0100

    script fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var inlinesource = require('gulp-inline-source');
  
 gulp.task('inlinesource', function () {
-	    return gulp.src('../kaputelefon/kaputelefon-frontend/src/*.html')
+	    return gulp.src('*.html')
 	        .pipe(inlinesource())
-	        .pipe(gulp.dest('../kaputelefon/kaputelefon-frontend/target'));
+	        .pipe(gulp.dest('../target'));
 });
