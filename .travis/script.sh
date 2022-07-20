343813076fcac76255326b5180e23e86d922404e
dobrosi.andras <dobrosi.andras@microsec.hu>
2022-07-20 11:09:36 +0200

    build

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,35 +1,15 @@
 #!/bin/sh
 
-setup() {
-  mkdir work
-  cd work
-  cp ../gulpfile.js . 
+build() {
   npm install -g gulp-cli
   npm install gulp --save-dev
   npm i gulp-inline-source
 
   gulp inlinesource
 
-  cd ..
-
-}
-
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
+  git config --global user.email "dobrosi@gmail.com"
   git config --global user.name "Travis CI"
+  git tag $TRAVIS_BUILD_NUMBER
 }
 
-commit() {
-  git remote set-url origin https://dobrosi:${GH_TOKEN}@github.com/dobrosi/jozsefutca.git
-  git add .
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-upload_files() {
-  git push origin master
-}
-
-setup
-setup_git
-commit
-upload_files
+build
--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,5 @@ var inlinesource = require('gulp-inline-source');
 gulp.task('inlinesource', function () {
 	    return gulp.src('*.html')
 	        .pipe(inlinesource())
-	        .pipe(gulp.dest('../target'));
+	        .pipe(gulp.dest('target'));
 });
